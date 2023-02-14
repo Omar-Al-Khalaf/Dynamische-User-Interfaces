5062134746bc06abb529db60e8b3f3d8190f3f5a
OmarAlKhalaf <99047548@mydavinci.nl>
2023-02-14 10:17:27 +0100

    Work is completed

--- Module-01/m01o1.js
+++ Module-01/m01o1.js
@@ -1,9 +1,14 @@
 // Function //
 function calculate(){
-    let text = document.getElementById('userinput');
-    document.getElementById('user_Result').innerHTML = text;
+    let text = document.getElementById('userinput').value;
+    outcomst = Math.ceil((text - 80) / 120);
+    document.getElementById('user_Result').innerHTML = outcomst
 };
+
 // Alert //     
 alert()
 // Console //
 console.log()
+
+// In een parkeergarage passen 80 auto's op de begane grond en 120 op een verdieping.\n"+
+// "Vraag het gewenste aantal auto's in de garage en bereken het aantal verdiepingen wat je nodig hebt."
